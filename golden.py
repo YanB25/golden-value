c271c7cb00c4b96969570c5acf880026b38fdb89
YanB25 <yanb25@mail2.sysu.edu.cn>
2018-08-20 22:26:31 +0800

    trivial

--- golden.py
+++ golden.py
@@ -1,9 +1,9 @@
 history = []
 def read():
-    line, cnt = input().split('    ')
+    line, cnt = input().split('    ') # 读第一行的两个数字
     line, cnt = (int(line), int(cnt))
     for _ in range(line):
-        goldValue, *playerValues = input().strip().split('\t')
+        goldValue, *playerValues = input().strip().split('\t') # 读第i行的内容
         history.append({
             'goldValue': float(goldValue),
             'playValues': list(map((lambda x:float(x)), playerValues))
@@ -12,9 +12,13 @@ def read():
 def show():
     print(history)
 
+def calculate():
+    return 50*0.618, 50*0.618
+
 def main():
     read()
-    show()
+    ret = calculate()
+    print(ret[0], ret[1], end='\t')
 
 if __name__ == "__main__":
     main()
