f562dd2b6d5cfcd27ec9801b98b9f7cde98a9f3b
YanB25 <yanb25@mail2.sysu.edu.cn>
2018-08-21 01:26:41 +0800

    finish

--- golden.py
+++ golden.py
@@ -1,3 +1,4 @@
+from random import *
 history = []
 def read():
     line, cnt = input().split('    ') # 读第一行的两个数字
@@ -13,12 +14,22 @@ def show():
     print(history)
 
 def calculate():
-    return 50*0.618, 50*0.618
+    acculateValue = 0
+    alpha = 0.8
+    for historyData in history:
+        gv = historyData['goldValue']
+        acculateValue = acculateValue * (1-alpha) + gv * alpha
+    if random() <= 0.39:
+        err = random() * 10 + 85
+        return err, acculateValue + err/46*0.618
+    else:
+        return acculateValue, acculateValue
 
 def main():
     read()
     ret = calculate()
-    print(ret[0], ret[1], end='\t')
+    print('{:f}\t{:f}'.format(ret[0], ret[1]))
+
 
 if __name__ == "__main__":
     main()
