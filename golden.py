d7bed2cc1e0d50a4808335e26ddf08e5221dd08a
YanB25 <yanb25@mail2.sysu.edu.cn>
2018-08-21 02:12:14 +0800

    fix

--- golden.py
+++ golden.py
@@ -14,6 +14,8 @@ def show():
     print(history)
 
 def calculate():
+    if len(history) <= 3:
+        return random() * 3 + 20, random()*5 + 20
     acculateValue = 0
     alpha = 0.8
     for idx, historyData in enumerate(history):
@@ -21,17 +23,15 @@ def calculate():
         acculateValue = acculateValue * (1-alpha) + gv * alpha
     if (abs(history[-1]['goldValue']-history[-2]['goldValue'] <= 1)) or random() <= 0.25:
         err = random() * 10 + 85
-        return err, acculateValue + err/(2*cnt)*0.618
+        return err, acculateValue + err/(2*23)*0.618
     else:
         return acculateValue, acculateValue
 
 def main():
-    try:
-        read()
-        ret = calculate()
-        print('{:.2f}\t{:.2f}'.format(ret[0], ret[1]))
-    except:
-        print('{:.2f}\t{:.2f}'.format(random() * 3 + 20, random()*5 + 20))
+    read()
+    ret = calculate()
+    print('{:.2f}\t{:.2f}'.format(ret[0], ret[1]))
+        #print('{:.2f}\t{:.2f}'.format(random() * 3 + 20, random()*5 + 20))
 
 
 
